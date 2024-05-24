452fdf3fd237d04cb173f306e15033408eb0ed5c
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-05-24 20:45:39 -0300

    feat: add page favorites

---
+++ src/app/favorites/favorites.page.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Pokemon, PokemonService} from "../api/pokemon.service";
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.page.html',
+  styleUrls: ['./favorites.page.scss'],
+})
+export class FavoritesPage implements OnInit {
+  public favorites: Pokemon[] = [];
+
+  constructor(private pokemonService: PokemonService) {
+  }
+
+  ngOnInit() {
+    this.loadFavorites();
+  }
+
+  public loadFavorites() {
+    this.pokemonService.getFavorites().subscribe(favorites => this.favorites = favorites)
+  }
+
+  public removeFavorite(pokemon: Pokemon) {
+    this.pokemonService.removeFavorite(pokemon);
+    this.loadFavorites();
+  }
+}
